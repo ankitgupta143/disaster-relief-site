a485eb25315fd5c27a64531ca61c835f3878ff57
Ankit Gupta <114281117+ankitgupta143@users.noreply.github.com>
2025-04-29 14:39:52 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-  apiKey: "AIzaSyBHqILdemr8fVLrThOTdnvG2cF8IpM-hJo",
-  authDomain: "nexus-8487a.firebaseapp.com",
-  projectId: "nexus-8487a",
-  storageBucket: "nexus-8487a.appspot.com",
-  messagingSenderId: "393295080767",
-  appId: "1:393295080767:web:d4173802628ecb810fb78a",
-  measurementId: "G-E7F1N4MBCK",
+  apiKey: "ADD YOUR",
+  authDomain: "ADD YOUR,
+  projectId: "ADD YOUR",
+  storageBucket: "ADD YOUR,
+  messagingSenderId: "ADD YOUR",
+  appId: "ADD YOUR",
+  measurementId: "ADD YOUR",
 };
 
 // Initialize Firebase
